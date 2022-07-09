08e0c2ca6b38ad66122cdf6fa1247b24952ef409
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-09 14:53:19 +1200

    config server to link internal api to prompts route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const path = require('path')
 
 const server = express()
+//filepath to the prompts route
+const prompts = require('./routes/prompts')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+//internal api
+server.use('/api/v1/prompts', prompts)
 
 module.exports = server
