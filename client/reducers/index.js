9a6f77a450b090f8e7e9ab69f6480b0010f91b75
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-10 21:02:16 +1200

    add err message reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import promptsReducer from './prompts'
+import errorReducer from './errMessage'
 
 // import stuff from './stuff'
 
 export default combineReducers({
   prompts: promptsReducer,
+  errMessage: errorReducer,
 })
