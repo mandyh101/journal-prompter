803e2097c84377fd4a37e9f9bec7d47a65f83cd0
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-11 10:20:02 +1200

    update create store to legacy createstore. need to update to configureStore

--- client/index.js
+++ client/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { legacy_createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const store = legacy_createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
