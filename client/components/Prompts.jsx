558d067b78d172f1268f9fea29d1f7adf38cff4b
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-11 10:47:05 +1200

    add place holders for error message and loadin indicators

--- client/components/Prompts.jsx
+++ client/components/Prompts.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchPrompts } from "../actions/prompts";
  
@@ -13,8 +12,10 @@ function Prompts() {
   }, [])
 
   return (  
-    <div>
+    <div className="prompts-container">
+    {/* insert error message component here */}
     <h1>Hello Journal Prompts!</h1>
+    {/* insert loading indicator here */}
     <ul>
       {prompts.map((prompt) => (
         <li key={prompt.id}>{prompt.prompt}</li>
