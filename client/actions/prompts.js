45fb3fdf0f85dda946e439a3ad73b290116ceb44
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-11 12:57:07 +1200

    build addPrompt action creator

--- client/actions/prompts.js
+++ client/actions/prompts.js
@@ -3,7 +3,14 @@ import { getPromptsData } from "../apis/promptsApi"
 export const SET_PROMPTS = 'SET_PROMPT'
 export const SET_PROMPTS_PENDING = 'SET_PROMPTS_PENDING'
 export const SET_ERROR = 'SET_ERROR'
+export const ADD_PROMPT = 'ADD_PROMPT'
 
+export const addPrompt = (newPrompt) => {
+  return {
+    type: newPrompt
+    //next step is to update payload to include category
+  }
+}
 
 export function fetchPrompts () {
   return (dispatch) => {
