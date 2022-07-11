d999001a4a582c9e96fbc5d0c837cb0d54627270
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-11 16:19:06 +1200

    complete form and call action creator with local state on submit

--- client/components/AddPrompt.jsx
+++ client/components/AddPrompt.jsx
@@ -1,34 +1,51 @@
 import React, {useState} from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { savePrompt } from "../actions/prompts";
 
-//uncomment below import once action creator is set up
-//*import {addPrompt} from '../actions/prompts' 
+
+const initialForm = {
+  id: '',
+  prompt: '',
+  category: '',
+}
 
 function AddPrompt () {
-  //*const prompts = useSelector(state => state.prompts)
 
   //create local state to control form input
-  const [newPrompt, setNewPrompt] = useState('') //?does this need to be an empty object to capture both the prompt and teh category?
+  const [newPrompt, setNewPrompt] = useState(initialForm) //?does this need to be an empty object to capture both the prompt and teh category?
 
-  //*const dispatch = useDispatch()
+  const dispatch = useDispatch()
+  // const prompts = useSelector(state => state.prompts)
+  // console.log(prompts)
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(`You want to save this new prompt: ${newPrompt}`)
-    //dispatch(addPrompt(newPrompt))
-    setNewPrompt('')
-    //once action is created, replace line 14 with dispatch(addPrompt(newPrompt)) nd then setNewPrompt back to empty string
+  function handleSubmit(evt) {
+    evt.preventDefault()
+    dispatch(savePrompt(newPrompt))
+    // clear all fields
+    setNewPrompt(initialForm)
   }
 
-  function handleChange(e){
-    setNewPrompt(e.target.value)
+  function handleChange(evt) {
+    // this comes from name="fieldName" in the input
+    console.log(evt.target.name)
+    // this comes from what the user typed
+    console.log(evt.target.value)
+
+    setNewPrompt({
+      // copy the rest of the fields
+      ...newPrompt,
+      // overwrite just the one we care about
+      [evt.target.name]: evt.target.value,
+    })
   }
 
   // next step is to add another input for category
   return ( 
     <form onSubmit={handleSubmit}>
       <label htmlFor="promptInput">Add a new journal prompt!</label>
-      <input id="promptInput" type="text" name="promptInput" value={newPrompt} onChange={handleChange}></input>
+      <input id="promptInput" type="text" name="prompt" value={newPrompt.prompt} onChange={handleChange}></input>
+      <label htmlFor="category">Add a new prompt category</label>
+      <input id="category" type="text" name="category" value={newPrompt.category} onChange={handleChange}></input>
       <input type='submit' value='Create' />
     </form>
    );
