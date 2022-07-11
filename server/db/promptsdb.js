0a89d86f2e360337e8652212f615524fcda620b2
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-11 19:05:19 +1200

    write db query to add new prompt

--- server/db/promptsdb.js
+++ server/db/promptsdb.js
@@ -6,7 +6,15 @@ const connection = require('knex')(config)
 function getAllPrompts(db = connection) {
   return db('prompts').select()
 }
+function addPrompt(newPrompt, db = connection) {
+  const { prompt, category} = newPrompt //deconstruct the newPrompt obj
+  return db('prompts')
+    .insert({prompt, category})
+    .then(([id]) => {return {id, prompt, category} //not sure what thi sline is doing?
+    })
+}
 
 module.exports = {
   getAllPrompts,
+  addPrompt
 }
