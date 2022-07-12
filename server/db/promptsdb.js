7779b94382ae2bf8de156fdaa32a529a8e0638b7
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-12 14:25:45 +1200

    pseudocode to delete prompt in database

--- server/db/promptsdb.js
+++ server/db/promptsdb.js
@@ -6,15 +6,28 @@ const connection = require('knex')(config)
 function getAllPrompts(db = connection) {
   return db('prompts').select()
 }
+
 function addPrompt(newPrompt, db = connection) {
-  const { prompt, category} = newPrompt //deconstruct the newPrompt obj
-  return db('prompts')
-    .insert({prompt, category})
-    .then(([id]) => {return {id, prompt, category} //not sure what thi sline is doing?
-    })
+  const { prompt, category } = newPrompt //deconstruct the newPrompt obj
+  return (
+    db('prompts')
+      .insert({ prompt, category })
+      // .then(([id]) => {return {id, prompt, category} //shorter way
+      .then((ids) => {
+        return { id: ids[0], prompt, category } //with the array returned from insert , insert an object with the id property = to the id array
+        //.then(ids) => {
+        // return getPrompt(ids[0])}
+      })
+  )
 }
+
+//function delPrompt(promptId, db= dbconnection){
+//   return db('prompts').where('id', promptId).delete()
+// }
+
+// return db('coffee-beans').where('id', coffeeId).delete()
 
 module.exports = {
   getAllPrompts,
-  addPrompt
+  addPrompt,
 }
--- server/routes/prompts.js
+++ server/routes/prompts.js
@@ -27,4 +27,24 @@ router.post('/', (req, res) => {
       res.status(500).send('Server error')
     })
 })
+
+//router.delete('/', (req,res) => {
+  // const id = req.body.id
+  //call the db func that deletes(id)
+  //.then(deletedPrompt)
+// })
 module.exports = router
+
+
+
+
+// router.delete('/', (req, res) => {
+//   const id = req.body.id
+//   db.deleteCoffee(id)
+//     .then(() => {
+//       res.sendStatus(200)
+//     })
+//     .catch((err) => {
+//       res.status(500).send(err.message)
+//     })
+// })
