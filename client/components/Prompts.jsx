c11c8831b473a0ab5c4f06aeb4b858e0bdb2cf2a
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-17 11:07:50 +1200

    fix styles path plus some debugging logs

--- client/components/Prompts.jsx
+++ client/components/Prompts.jsx
@@ -4,35 +4,31 @@ import { useNavigate } from "react-router-dom";
 import { fetchPrompts , removePrompt } from "../actions/prompts";
 
 import ErrorMessage from "./ErrorMessage";
-import Pending from "./Pending";
-import styles from "./Prompts.module.css"
+//import Pending from "./Pending";
+import styles from "../styles/Prompts.module.scss"
  
 
 function Prompts() {
   const prompts = useSelector(state => state.prompts)
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  console.log('prompts' , prompts)
+
 
   useEffect(() => {
-    setTimeout(() => {
       dispatch(fetchPrompts())
-    }, 50)
-    
   }, [])
 
   function deletePrompt(id){
     dispatch(removePrompt(id))
   }
 
-  function updatePrompt(prompt){
-    dispatch(updatePrompt(prompt))
-  }
 
   return (  
     <div className={styles.Prompts}>
     <ErrorMessage />
     <h1>Hello Journal Prompts!</h1>
-    <Pending />
+    {/* <Pending /> */}
     <ul>
       {prompts.map((prompt) => (
         <li key={prompt.id}>
@@ -40,11 +36,8 @@ function Prompts() {
           <div className="category">
             {prompt.category}
           </div>
-          {/* this button needs to change to a link and take user to an edit page */}
-            {/* <button onClick={() => updatePrompt(prompt)}>Update</button>  */}
-            <button onClick={()=>navigate('/edit/'+prompt.id)}>Edit this prompt</button>
-
-            <button onClick={() => deletePrompt(prompt.id)}>Delete</button>
+          <button onClick={()=>navigate('/edit/'+prompt.id)}>Edit this prompt</button>
+          <button onClick={() => deletePrompt(prompt.id)}>Delete</button>
         </li>
       ))}
     </ul>
