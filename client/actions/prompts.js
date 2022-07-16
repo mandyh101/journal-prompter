c2da968506348333445276a60518f1223d7223bd
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-16 21:36:44 +1200

    write action to fetch one prompt

--- client/actions/prompts.js
+++ client/actions/prompts.js
@@ -2,19 +2,49 @@ import {
   addNewPrompt,
   getPromptsData,
   removePromptApi,
+  editPromptApi,
+  getPromptById,
 } from '../apis/promptsApi'
 
-export const SET_PROMPTS = 'SET_PROMPT'
+export const SET_PROMPTS = 'SET_PROMPTS'
+export const SET_PROMPT = 'SET_PROMPT'
 export const SET_PROMPTS_PENDING = 'SET_PROMPTS_PENDING'
 export const SET_ERROR = 'SET_ERROR'
 export const ADD_PROMPT = 'ADD_PROMPT'
 export const DEL_PROMPT = 'DEL_PROMPT'
 export const UPDATE_PROMPT = 'UPDATE_PROMPT'
 
-export function updatePromptCategory(prompt) {
+export function updatePrompt(prompt) {
   return {
     type: UPDATE_PROMPT,
     payload: prompt,
+  }
+}
+
+export function editPrompt(prompt) {
+  return (dispatch) => {
+    // dispatch(setPromptsPending())
+    return editPromptApi(prompt)
+      .then((updatedPrompt) => {
+        dispatch(updatePrompt(updatedPrompt))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+export function fetchPrompt(id) {
+  return (dispatch) => {
+    return getPromptById(id)
+      .then((prompt) => {
+        dispatch(setPrompt(prompt))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
   }
 }
 
@@ -71,23 +101,23 @@ export function addPrompt(newPrompt) {
   }
 }
 
-// export const postPrompt = (prompt) => {
-//   return {
-//     type: POST_PROMPT,
-//     payload: prompt,
-//   }
-// }
-
 export function setPromptsPending() {
   return {
     type: SET_PROMPTS_PENDING,
   }
 }
 
-export const setPrompts = (prompt) => {
+export const setPrompt = (prompt) => {
+  return {
+    type: SET_PROMPT,
+    payload: prompt,
+  }
+}
+
+export const setPrompts = (prompts) => {
   return {
     type: SET_PROMPTS,
-    payload: prompt,
+    payload: prompts,
   }
 }
 
