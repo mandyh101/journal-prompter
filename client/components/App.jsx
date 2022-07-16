9f6317fe6b88afeeddecf3c7bdc05b42ddcfd692
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-16 19:39:39 +1200

    add route path for prompts on home and for edit prompts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import AddPrompt from './AddPrompt'
+import EditPrompt from './EditPrompt'
 import Prompts from './Prompts'
 
 function App () {
   return (
     <>
-      <header className="header">
-        <h1>My Collection</h1>
-      </header>
       <section className="main">
+        <header className="header">  {/*//TODO make Header into component */}
+          <h1>My Collection</h1>
+        </header>
+        <Routes>
+          <Route path='/' element={<Prompts />}/>
+          <Route path='/edit/:id' element={<EditPrompt />}/>
+        </Routes>
         <AddPrompt />
-        <Prompts />
+        
       </section>
     </>
   )
