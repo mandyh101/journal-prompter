f7bab38e00a13d7599635833b2654f655629e663
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-16 20:14:25 +1200

    pseudocode useffect , bring in useDispatch and useParams

--- client/components/EditPrompt.jsx
+++ client/components/EditPrompt.jsx
@@ -1,19 +1,39 @@
 import React, {useState} from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function EditPrompt () {
   
-  const prompt = useSelector((state) => state.prompt)
+  const prompt = useSelector((state) => state.prompts)
+  const [editPromptInput, setEditPromptInput] = useState({})
+  const dispatch = useDispatch()
+
+  function handleOnChange(e){
+    setEditPromptInput({
+      ...editPromptInput,
+      [e.target.name]: e.target.value
+    })
+    console.log('edited prompt', editPromptInput)
+  }
 
   return ( 
     <form>
       <h3>Edit prompt</h3>
       <label htmlFor="prompt"> Edit journal prompt:
-        <textarea id="prompt" name="prompt" type="text">
+        <textarea 
+        id="prompt" 
+        name="prompt" 
+        type="text" 
+        defaultValue={prompt.prompt}
+        onChange={handleOnChange}>
         </textarea>
       </label>
       <label htmlFor="category"> Edit journal category:
-        <input id="category" name="category" type="text">
+        <input 
+        id="category" 
+        name="category" 
+        type="text" 
+        defaultValue={prompt.category}
+        onChange={handleOnChange}>
         </input>
       </label>
       <button type="submit"></button>
