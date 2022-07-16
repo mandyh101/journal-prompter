73d8533b9001f33ccb75f511b5d0735817e9bcc0
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-17 11:10:21 +1200

    move set prompt and update prompt to single prompt reducer

--- client/reducers/prompts.js
+++ client/reducers/prompts.js
@@ -2,38 +2,38 @@
 import {
   ADD_PROMPT,
   SET_PROMPTS,
-  SET_PROMPT,
+  // SET_PROMPT,
   DEL_PROMPT,
-  UPDATE_PROMPT,
+  // UPDATE_PROMPT,
 } from '../actions/prompts'
 
 const initialPromptState = []
 
-const promptsReducer = (state = initialPromptState, action) => {
+const prompts = (state = initialPromptState, action) => {
   const { type, payload } = action
 
   switch (type) {
     case SET_PROMPTS:
       return payload
-    case SET_PROMPT:
-      return payload
+    // case SET_PROMPT:
+    //   return payload
     case ADD_PROMPT:
       return [...state, payload] //add new prompt to state array
     case DEL_PROMPT:
       //might need to create a new Array to persist the state change
       return state.filter((prompt) => prompt.id !== payload)
-    case UPDATE_PROMPT:
-      return state.map((prompt) => {
-        //map over the prompts
-        if (prompt.id === payload.id) {
-          // if prompt id matches the prompt id of the payload
-          return { ...prompt, ...payload } //return the prompt but overwrite it with any new data in the payload.
-        }
-        return prompt
-      })
+    // case UPDATE_PROMPT:
+    //   return state.map((prompt) => {
+    //     //map over the prompts
+    //     if (prompt.id === payload.id) {
+    //       // if prompt id matches the prompt id of the payload
+    //       return { ...prompt, ...payload } //return the prompt but overwrite it with any new data in the payload.
+    //     }
+    //     return prompt
+    //   })
     default:
       return state
   }
 }
 
-export default promptsReducer
+export default prompts
