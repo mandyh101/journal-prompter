9d349876bbe48332a54957eb19eb5497e6158e54
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-17 12:30:22 +1200

    tidy code

--- client/actions/prompts.js
+++ client/actions/prompts.js
@@ -14,40 +14,19 @@ export const ADD_PROMPT = 'ADD_PROMPT'
 export const DEL_PROMPT = 'DEL_PROMPT'
 export const UPDATE_PROMPT = 'UPDATE_PROMPT'
 
-export function updatePrompt(prompt) {
+//READ
+
+export const setPrompts = (prompts) => {
   return {
-    type: UPDATE_PROMPT,
+    type: SET_PROMPTS,
+    payload: prompts,
+  }
+}
+
+export const setPrompt = (prompt) => {
+  return {
+    type: SET_PROMPT,
     payload: prompt,
-  }
-}
-
-export function editPrompt(prompt, id) {
-  return (dispatch) => {
-    // dispatch(setPromptsPending())
-    return editPromptApi(prompt, id)
-      .then(() => {
-        console.log('action', prompt)
-        dispatch(updatePrompt(prompt))
-        return null
-      })
-      .catch((err) => {
-        dispatch(setError(err.message))
-      })
-  }
-}
-
-export function fetchPrompt(id) {
-  return (dispatch) => {
-    console.log('action', id)
-    return getPromptById(id)
-      .then((prompt) => {
-        console.log('action', prompt)
-        dispatch(setPrompt(prompt))
-        return null
-      })
-      .catch((err) => {
-        dispatch(setError(err.message))
-      })
   }
 }
 
@@ -65,6 +44,52 @@ export function fetchPrompts() {
   }
 }
 
+export function fetchPrompt(id) {
+  return (dispatch) => {
+    console.log('action', id)
+    return getPromptById(id)
+      .then((prompt) => {
+        dispatch(setPrompt(prompt))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+//UPDATE
+
+export function updatePrompt(prompt) {
+  return {
+    type: UPDATE_PROMPT,
+    payload: prompt,
+  }
+}
+
+export function editPrompt(prompt, id) {
+  return (dispatch) => {
+    return editPromptApi(prompt, id)
+      .then(() => {
+        console.log('action', prompt)
+        dispatch(updatePrompt(prompt))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+//CREATE
+
+export function addPrompt(newPrompt) {
+  return {
+    type: ADD_PROMPT,
+    payload: newPrompt,
+  }
+}
+
 export function savePrompt(newPrompt) {
   return (dispatch) => {
     return addNewPrompt(newPrompt)
@@ -75,6 +100,15 @@ export function savePrompt(newPrompt) {
       .catch((err) => {
         dispatch(setError(err.message))
       })
+  }
+}
+
+//DELETE
+
+export const deletePrompt = (promptId) => {
+  return {
+    type: DEL_PROMPT,
+    payload: promptId,
   }
 }
 
@@ -90,37 +124,11 @@ export function removePrompt(promptId) {
   }
 }
 
-export const deletePrompt = (promptId) => {
-  return {
-    type: DEL_PROMPT,
-    payload: promptId,
-  }
-}
-
-export function addPrompt(newPrompt) {
-  return {
-    type: ADD_PROMPT,
-    payload: newPrompt,
-  }
-}
+//HELPER ACTIONS
 
 export function setPromptsPending() {
   return {
     type: SET_PROMPTS_PENDING,
-  }
-}
-
-export const setPrompt = (prompt) => {
-  return {
-    type: SET_PROMPT,
-    payload: prompt,
-  }
-}
-
-export const setPrompts = (prompts) => {
-  return {
-    type: SET_PROMPTS,
-    payload: prompts,
   }
 }
 
