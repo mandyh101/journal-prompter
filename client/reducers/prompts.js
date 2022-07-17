8f1b2f345457e5e33be4272c199a861f76994598
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-17 12:27:19 +1200

    tidy up code

--- client/reducers/prompts.js
+++ client/reducers/prompts.js
@@ -1,11 +1,5 @@
-//import action creator from actions
-import {
-  ADD_PROMPT,
-  SET_PROMPTS,
-  // SET_PROMPT,
-  DEL_PROMPT,
-  // UPDATE_PROMPT,
-} from '../actions/prompts'
+//import action creators from actions
+import { ADD_PROMPT, SET_PROMPTS, DEL_PROMPT } from '../actions/prompts'
 
 const initialPromptState = []
 
@@ -15,22 +9,10 @@ const prompts = (state = initialPromptState, action) => {
   switch (type) {
     case SET_PROMPTS:
       return payload
-    // case SET_PROMPT:
-    //   return payload
     case ADD_PROMPT:
       return [...state, payload] //add new prompt to state array
     case DEL_PROMPT:
-      //might need to create a new Array to persist the state change
-      return state.filter((prompt) => prompt.id !== payload)
-    // case UPDATE_PROMPT:
-    //   return state.map((prompt) => {
-    //     //map over the prompts
-    //     if (prompt.id === payload.id) {
-    //       // if prompt id matches the prompt id of the payload
-    //       return { ...prompt, ...payload } //return the prompt but overwrite it with any new data in the payload.
-    //     }
-    //     return prompt
-    //   })
+      return state.filter((prompt) => prompt.id !== payload) //return prompts array but filter out deleted prompt
     default:
       return state
   }
