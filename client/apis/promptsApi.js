455223f15f57f170c985b77393287c29eb62ad58
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-17 12:34:01 +1200

    organise code and add helper comments

--- client/apis/promptsApi.js
+++ client/apis/promptsApi.js
@@ -1,6 +1,9 @@
-import request, { get } from 'superagent'
+import request from 'superagent'
 const promptApi = '/api/v1/prompts/'
 
+//READ
+
+//GET api/v1/prompts/
 export function getPromptsData() {
   return request
     .get(promptApi)
@@ -13,6 +16,25 @@ export function getPromptsData() {
     })
 }
 
+//GET api/v1/prompts/:id
+export function getPromptById(id) {
+  return request
+    .get(promptApi + id)
+    .then((res) => {
+      const prompt = {
+        prompt: res.body.prompt,
+        category: res.body.category,
+      }
+      return prompt
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+//CREATE
+
+//GET api/v1/prompts/ add?
 export function addNewPrompt(newPrompt) {
   return request
     .post(promptApi)
@@ -25,6 +47,9 @@ export function addNewPrompt(newPrompt) {
     })
 }
 
+//DELETE
+
+//DELETE api/v1/prompts/
 export function removePromptApi(promptId) {
   console.log('hit delete route', promptId)
   return (
@@ -40,20 +65,7 @@ export function removePromptApi(promptId) {
   )
 }
 
-export function getPromptById(id) {
-  return request
-    .get(promptApi + id)
-    .then((res) => {
-      const prompt = {
-        prompt: res.body.prompt,
-        category: res.body.category,
-      }
-      return prompt
-    })
-    .catch((err) => {
-      console.error(err)
-    })
-}
+//UPDATE
 
 //api/v1/prompts/edit/:id
 export function editPromptApi(prompt, id) {
